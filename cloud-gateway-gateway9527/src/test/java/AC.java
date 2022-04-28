b930c509bf70e1df09b06bbd5020d0e9011a7f64
Bing <3521315291@qq.com>
2022-04-28 12:26:26 +0800

    test2

--- cloud-gateway-gateway9527/src/test/java/AC.java
+++ cloud-gateway-gateway9527/src/test/java/AC.java
@@ -4,6 +4,7 @@ public class AC {
     public static void main(String[] args) {
         ZonedDateTime now = ZonedDateTime.now();
         System.out.println(now);
+        System.out.println(now);
         // 2022-04-28T10:37:28.040+08:00[Asia/Shanghai]
     }
 }
