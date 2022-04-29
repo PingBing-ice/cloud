03a76f63b4b3c91d0b72a0675039e6a0b3a3cc1f
Bing <3521315291@qq.com>
2022-04-29 17:44:14 +0800

    cloudTest4

--- cloud-config-client-3366/src/main/java/com/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -1,18 +1,20 @@
 package com.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope  // 端口刷新
 public class ConfigClientController {
-    @Value("${config.info}")   // 这里是读取仓库的配置文件
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
-    public String getConfigInfo() {
-        return configInfo;
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
     }
 }
